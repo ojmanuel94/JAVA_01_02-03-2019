0fce2a4acfbd01f467634889b4f585e8006bd56b
ojamnuel94 <ojmanuel94@gmail.com>
2019-02-03 12:54:57 +0800

    exercise 3 & 4

---
+++ GradingSystem.java
@@ -0,0 +1,17 @@
+
+public class GradingSystem {
+	public static void main (String[] args) {
+		int grade = 90;
+
+		if ( grade > 60){
+			System.out.println("Congratulations!");
+		}
+
+		else if ( grade > 90) 
+
+			System.out.println("MUNTIKAN NA!");
+
+		else 
+			System.out.println("BAGSAK KA! WAWA KA NAMAN!");
+	}
+}
